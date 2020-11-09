dfe4b2230123465653de9798d86195e4eccf96c5
andhmak <sdi1900047@di.uoa.gr>
2020-11-09 16:21:41 +0200

    Constr/destr

--- exer1/a.cpp
+++ exer1/a.cpp
@@ -5,12 +5,19 @@
 
 using namespace std;
 
+enum area {
+    at_yard, at_stairs, at_corridor, at_class, outside
+};
+
 class Class {
     int capacity;
     int student_num;
     public:
         Class(int init_capacity) : capacity(init_capacity) {
             cout << "A New Class has been created!" << endl;
+        }
+        ~Class() {
+            cout << "A Class to be destroyed!" << endl;
         }
 };
 
@@ -21,6 +28,9 @@ class Corridor {
         Corridor(int init_capacity) : capacity(init_capacity) {
             cout << "A New Corridor has been created!" << endl;
         }
+        ~Corridor() {
+            cout << "A Corridor to be destroyed!" << endl;
+        }
 };
 
 class Yard {
@@ -29,6 +39,9 @@ class Yard {
     public:
         Yard(int init_capacity) : capacity(init_capacity) {
             cout << "A New Yard has been created!" << endl;
+        }
+        ~Yard() {
+            cout << "A Yard to be destroyed!" << endl;
         }
 };
 
@@ -39,31 +52,85 @@ class Stairs {
         Stairs(int init_capacity) : capacity(init_capacity) {
             cout << "New Stairs have been created!" << endl;
         }
+        ~Stairs() {
+            cout << "Stairs to be destroyed!" << endl;
+        }
 };
 
 class Floor {
-    Class classes;
+    Class* classes[6];
     Corridor corridor;
     public:
-        Floor(int cclass, int ccorr) : classes(cclass), corridor(ccorr) {
+        Floor(int cclass, int ccorr) : corridor(ccorr) {
+            for (int i = 0 ; i < 3 ; i++) {
+                classes[i] = new Class(cclass);
+            }
             cout << "A New Floor has been created!" << endl;
+        }
+        ~Floor() {
+            cout << "A Floor to be destroyed!" << endl;
+            for (int i = 0 ; i < 3 ; i++) {
+                delete classes[i];
+            }
         }
 };
 
 class School {
     Yard yard;
     Stairs stairs;
-    Floor floors;
+    Floor* floors[3];
     public:
         School(int cclass, int cyard, int cstair, int ccorr)
-        :   yard(cyard), stairs(cstair), floors(cclass, ccorr)
+        :   yard(cyard), stairs(cstair)
         {
+            for (int i = 0 ; i < 3 ; i++) {
+                floors[i] = new Floor(cclass, ccorr);
+            }
             cout << "A New School has been created!" << endl;
+        }
+        ~School() {
+            cout << "A School to be destroyed!" << endl;
+            for (int i = 0 ; i < 3 ; i++) {
+                delete floors[i];
+            }
         }
 };
 
 class Student {
     string name;
+    int floor_num;
+    int class_num;
+    enum area position;
+    public:
+        Student(char* init_name, int init_floor_num, int init_class_num)
+        :   name(init_name), floor_num(init_floor_num), class_num(init_class_num)
+        {
+            position = outside;
+            cout << "A New Student has been created!" << endl;
+            cout << name << endl;
+            cout << "Floor " << floor_num  << ", class " << class_num << endl;
+        }
+        ~Student() {
+            cout << "A Student to be destroyed!" << endl;
+            cout << name << endl;
+            cout << "Floor " << floor_num  << ", class " << class_num << endl;
+            switch (position) {
+                case at_yard:
+                    cout << "Situated in the yard" << endl;
+                    break;
+                case at_stairs:
+                    cout << "Situated on the stairs" << endl;
+                    break;
+                case at_corridor:
+                    cout << "Situated in the corridor" << endl;
+                    break;
+                case at_class:
+                    cout << "Situated in the class" << endl;
+                    break;
+                case outside:
+                    cout << "Situated outside" << endl;
+            }
+        }
 };
 
 int main(int argc, char* argv[]) {
