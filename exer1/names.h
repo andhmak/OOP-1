ea6db32ab6c88fea43434a445aa6d4f1323a5f47
andhmak <sdi1900047@di.uoa.gr>
2020-11-24 01:40:02 +0200

    save

--- exer1/names.h
+++ exer1/names.h
@@ -1,6 +1,6 @@
-const char* names[40] = {
-    "Oliver", "Jake", "Noah", "James", "Jack", "Connor", "Liam", "John", "Harry", "Callum",
-    "Mason", "Robert", "Jacob", "Michael", "Charlie", "Kyle", "William", "Thomas", "Joe", "Ethan",
-    "Amelia", "Margaret", "Emma", "Mary", "Olivia", "Samantha", "Patricia", "Isla", "Bethany", "Sophia",
-    "Jennifer", "Emily", "Elizabeth", "Isabella", "Ava", "Poppy", "Joanne", "Linda", "Megan", "Mia"
+const char* names[48] = {
+    "Oliver", "Jake", "Noah", "James", "Jack", "Connor", "Liam", "John", "Harry", "Callum", "Mason", "Robert",
+    "Jacob", "Michael", "Charlie", "Kyle", "William", "Thomas", "Joe", "Ethan", "David", "George", "Reece", "Richard",
+    "Amelia", "Margaret", "Emma", "Mary", "Olivia", "Samantha", "Patricia", "Isla", "Bethany", "Sophia", "Jennifer", "Emily",
+    "Elizabeth", "Isabella", "Ava", "Poppy", "Joanne", "Linda", "Megan", "Mia", "Barbara", "Victoria", "Susan", "Jessica"
 };
--- exer2/names.h
+++ exer2/names.h
@@ -1,8 +1,8 @@
-const char* male_names[20] = {
-    "Oliver", "Jake", "Noah", "James", "Jack", "Connor", "Liam", "John", "Harry", "Callum",
-    "Mason", "Robert", "Jacob", "Michael", "Charlie", "Kyle", "William", "Thomas", "Joe", "Ethan",
+const char* male_names[24] = {
+    "Oliver", "Jake", "Noah", "James", "Jack", "Connor", "Liam", "John", "Harry", "Callum", "Mason", "Robert",
+    "Jacob", "Michael", "Charlie", "Kyle", "William", "Thomas", "Joe", "Ethan", "David", "George", "Reece", "Richard",
 };
-const char* female_names[20] = {
-    "Amelia", "Margaret", "Emma", "Mary", "Olivia", "Samantha", "Patricia", "Isla", "Bethany", "Sophia",
-    "Jennifer", "Emily", "Elizabeth", "Isabella", "Ava", "Poppy", "Joanne", "Linda", "Megan", "Mia"
+const char* female_names[24] = {
+    "Amelia", "Margaret", "Emma", "Mary", "Olivia", "Samantha", "Patricia", "Isla", "Bethany", "Sophia", "Jennifer", "Emily",
+    "Elizabeth", "Isabella", "Ava", "Poppy", "Joanne", "Linda", "Megan", "Mia", "Barbara", "Victoria", "Susan", "Jessica"
 };
